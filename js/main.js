4d381371d599a725b2ee7e8eb11790dfa7846315
hyacint <stefan@MacBook-Pro.local>
2016-09-19 10:04:20 +0200

    boxes & slider

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
-function (dga) {
-fsdfdsfsd= dga
-  ;
-    }
+/*jshint esversion: 6 */
+
+var rangeInput = document.getElementById("slider1");
+
+rangeInput.addEventListener('mousemove', function() {
+  let newWidth=(600*this.value/100);
+  $(".d1").width(newWidth);
+});
+
+
+
+
